5c9cf7f9e5920f9ed8e243d776f7ec4d7f1ed48b
phantruongazapi <trungtruongphan116@gmail.com>
2025-01-13 14:35:28 +0700

    Update djasjd.lua

--- djasjd.lua
+++ djasjd.lua
@@ -1,33 +1,34 @@
+local HttpService = game:GetService("HttpService")
+
+local function loadScriptFromURL(url)
+    local success, result = pcall(function()
+        return HttpService:GetAsync(url)
+    end)
+    
+    if success then
+        local func, err = loadstring(result)
+        if func then
+            func()
+        else
+            warn("Lỗi khi tải và thực thi script: " .. err)
+        end
+    else
+        warn("Không thể tải script từ URL: " .. result)
+    end
+end
+
 local LocalScript = {}
 
 function LocalScript.runLocal()
-  local url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/LocalScript.lua"
-  
-  local HttpService = game:GetService("HttpService")
-  local scriptContent = HttpService:GetAsync(url)
-  
-  local func, err = loadstring(scriptContent)
-  if func then
-    func()  -- Thực thi mã Lua
-  else
-    warn("Lỗi khi tải và thực thi script: " .. err)
-  end
+    local url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/LocalScript.lua"
+    loadScriptFromURL(url)
 end
 
 local ServerScriptService = {}
 
 function ServerScriptService.runServer()
-  local url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/ServerScriptService.lua"
-  
-  local HttpService = game:GetService("HttpService")
-  local scriptContent = HttpService:GetAsync(url)
-  
-  local func, err = loadstring(scriptContent)
-  if func then
-    func()  -- Thực thi mã Lua
-  else
-    warn("Lỗi khi tải và thực thi script: " .. err)
-  end
+    local url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/ServerScriptService.lua"
+    loadScriptFromURL(url)
 end
 
 return {
