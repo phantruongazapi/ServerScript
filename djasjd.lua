f559fab83418b2da58fce63762c407f20e896e61
phantruongazapi <trungtruongphan116@gmail.com>
2025-01-13 14:25:09 +0700

    Update djasjd.lua

--- djasjd.lua
+++ djasjd.lua
@@ -1,13 +1,33 @@
 local LocalScript = {}
 
 function LocalScript.runLocal()
-  url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/LocalScript.lua"
+  local url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/LocalScript.lua"
+  
+  local HttpService = game:GetService("HttpService")
+  local scriptContent = HttpService:GetAsync(url)
+  
+  local func, err = loadstring(scriptContent)
+  if func then
+    func()  -- Thực thi mã Lua
+  else
+    warn("Lỗi khi tải và thực thi script: " .. err)
+  end
 end
 
 local ServerScriptService = {}
 
 function ServerScriptService.runServer()
-  url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/ServerScriptService.lua"
+  local url = "https://raw.githubusercontent.com/phantruongazapi/ServerScript/refs/heads/main/ServerScriptService.lua"
+  
+  local HttpService = game:GetService("HttpService")
+  local scriptContent = HttpService:GetAsync(url)
+  
+  local func, err = loadstring(scriptContent)
+  if func then
+    func()  -- Thực thi mã Lua
+  else
+    warn("Lỗi khi tải và thực thi script: " .. err)
+  end
 end
 
 return {
