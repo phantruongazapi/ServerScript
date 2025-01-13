3abd3672e208eae0eec7542b2cc5d5b7cbed9ffb
phantruongazapi <trungtruongphan116@gmail.com>
2025-01-13 14:30:21 +0700

    Update LocalScript.lua

--- LocalScript.lua
+++ LocalScript.lua
@@ -3,7 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local NotifyPlayerJoined = ReplicatedStorage:WaitForChild("NotifyPlayerJoined")
 
-local player = game.Players:GetPlayerByUserId(7698202440)
+local playerID = "7698202440"
 
 local function checkPlayers()
     for _, player in ipairs(Players:GetPlayers()) do
